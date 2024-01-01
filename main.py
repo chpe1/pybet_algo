8fe5c60d9e0e00d0e98b506a9f396ab00b27a4af
chpe1 <jeromenoiret@free.fr>
2024-01-01 23:03:46 +0100

    Adding a handicap according to the calendar

--- main.py
+++ main.py
@@ -1,72 +1,11 @@
 from nba_api.stats.endpoints import leaguestandingsv3
-from nba_api.stats.endpoints import scoreboard
-import dateparser
+import schedule
 import standing
 from datetime import date, datetime, timedelta, timezone
 
-# Date du jour
-current_date = date.today()
 
-# Afficher la date du jour
-print("Date du jour :", current_date)
-
-
-# Récupération des matchs du jour
-def get_nba_schedule(current_date):
-    calendar = []
-
-    # Appel à l'API à travers le module Scoreboard pour récupérer les infos sur les matchs à la date concernée
-    games = scoreboard.Scoreboard(
-        day_offset=0, game_date=current_date).get_data_frames()[0]
-
-    # Pour chacun des matchs du jour
-    for index, row in games.iterrows():
-        # On récupère la date et l'heure du match qu'on transforme en objet datetime.
-        date_match = row['GAME_DATE_EST'].split('T')[0]
-        heure_match = row['GAME_STATUS_TEXT']
-        date_obj = dateparser.parse(f"{date_match} {heure_match}")
-        # On convertit la date en timestamp
-        timestamp = int(date_obj.replace(tzinfo=timezone.utc).timestamp())
-        # On cré un dictionnaire avec uniquement les informations dont on a besoin
-        game_info = {
-            'timestamp': timestamp,
-            'team_home': row['HOME_TEAM_ID'],
-            'team_visitor': row['VISITOR_TEAM_ID']
-        }
-        # On ajoute ce match au calendrier
-        calendar.append(game_info)
-
-    return calendar
-
-
-def get_team_records():
-    # Obtenir les classements actuels des équipes
-    standings = leaguestandingsv3.LeagueStandingsV3()
-
-    # Obtenir les données des équipes
-    team_data = standings.get_normalized_dict()["Standings"]
-
-    # Créer un dictionnaire pour stocker les informations sur les équipes
-    team_records = {}
-
-    # Parcourir les données des équipes
-    for team_stats in team_data:
-        team_id = team_stats["TeamID"]
-        team_name = team_stats["TeamName"]
-        wins = team_stats["WINS"]
-        losses = team_stats["LOSSES"]
-        home = team_stats["HOME"]
-        road = team_stats["ROAD"]
-
-        # Stocker les informations dans le dictionnaire
-        team_records[team_id] = {
-            "Team_Name": team_name, "Wins": wins, "Losses": losses, "Home": home, "Road": road}
-
-    return team_records
-
-
-def pourcentage(match_data, team_records):
-    # Calculer le pourcentage de victoire à domicile et à l'extérieur pour chaque équipe
+def percentage(match_data, team_records):
+    # Calcul du pourcentage de victoire à domicile et à l'extérieur pour chaque équipe
     for match in match_data:
         home_team_id = match['team_home']
         visitor_team_id = match['team_visitor']
@@ -79,7 +18,6 @@ def pourcentage(match_data, team_records):
             road_losses = int(
                 team_records[visitor_team_id]["Road"].split('-')[1])
 
-            # Calculer le pourcentage de victoire
             home_win_percentage = home_wins / (home_wins + home_losses) * 100
             road_win_percentage = road_wins / (road_wins + road_losses) * 100
 
@@ -87,16 +25,26 @@ def pourcentage(match_data, team_records):
             eval_team[visitor_team_id] += road_win_percentage
 
 
-schedule = get_nba_schedule(current_date)
-eval_team = {team_id: 0 for game in schedule for team_id in [
+# Récupération des matchs du jour et des 4 jours précédents
+calendar, old_calendar = schedule.get_nba_schedule()
+
+# Récupération du classement NBA
+classement = standing.get_team_records()
+
+# Création d'un dictionnaire informant de l'évalutation attribuée à chaque équipe concernée -> nbr de points de chaque équipe initialisé à 0
+eval_team = {team_id: 0 for game in calendar for team_id in [
     game['team_home'], game['team_visitor']]}
 
+liste_teams = [team_id for game in calendar for team_id in [
+    game['team_home'], game['team_visitor']]]
 
-classement = get_team_records()
-pourcentage(schedule, classement)
+# Calcul du pourcentage de victoire (home ou away) pour chaque équipe concerné
+# Attribution du nombre de points équivalent au pourcentage à chaque équipe
+# Ajout de ce nombre de points à eval_team
+percentage(calendar, classement)
 
-
-# for team, data in classement.items():
-# eval_team['team'] =
+handicap_calendrier = schedule.handicap(liste_teams, old_calendar)
+for equipe_id, handicap in handicap_calendrier.items():
+    eval_team[equipe_id] += handicap
 
 print()
---
+++ schedule.py
@@ -0,0 +1,86 @@
+from datetime import timezone, timedelta, datetime
+from nba_api.stats.endpoints import scoreboard
+from nba_api.stats.static import teams
+import dateparser
+
+
+# Récupération des matchs du jour et des 4 jours précédents.
+def get_nba_schedule():
+    aujourdhui = datetime.now()
+    dates = []
+    for i in range(5):
+        date = (aujourdhui - timedelta(days=i)).strftime("%Y-%m-%d")
+        dates.append(date)
+
+    calendar = []
+    old_calendar = []
+
+    # Flag pour savoir si on est sur le premier indice de la liste dates (date du jour) => Dans ce cas, on mettra les matchs dans un dictionnaire à part
+    indice_dates = 0
+    for day in dates:
+        # Appel à l'API à travers le module Scoreboard pour récupérer les infos sur les matchs à la date concernée
+        games = scoreboard.Scoreboard(
+            day_offset=0, game_date=day).get_data_frames()[0]
+
+        # Pour chacun des matchs du jour
+        for index, row in games.iterrows():
+            # On récupère la date et l'heure du match qu'on transforme en objet datetime.
+            date_match = row['GAME_DATE_EST'].split('T')[0]
+            heure_match = row['GAME_STATUS_TEXT']
+            date_obj = dateparser.parse(f"{date_match} {heure_match}")
+            if date_obj is None:
+                date_obj = dateparser.parse(f"{date_match} 00:00:00")
+            # On convertit la date en timestamp
+            timestamp = int(date_obj.replace(tzinfo=timezone.utc).timestamp())
+            # On cré un dictionnaire avec uniquement les informations dont on a besoin
+            game_info = {
+                'timestamp': timestamp,
+                'date' : date_obj.strftime('%d-%m-%Y'),
+                'team_home': row['HOME_TEAM_ID'],
+                'team_visitor': row['VISITOR_TEAM_ID']
+            }
+            if indice_dates == 0:
+                # On ajoute ce match au calendrier
+                calendar.append(game_info)
+            else:
+                old_calendar.append(game_info)
+        indice_dates += 1
+
+
+    return calendar, old_calendar
+
+def handicap(liste_teams, four_last_days):
+    # Initialisation dunnombre de matchs à 1 pour l'équipe (puisqu'elle joue aujourd'hui de par sa présence dans liste_teams)
+    date_veille = (datetime.now() - timedelta(days=1)).strftime('%d-%m-%Y')
+    handicap = dict()
+    # Parcourir les équipes ayant un match aujourd'hui
+    for equipe_id in liste_teams:
+        # Pour débogage, affichage du nom de l'équipe
+        all_teams = teams.get_teams()
+        for team in all_teams:
+            if team['id'] == equipe_id:
+                print(team['full_name'] + '-' + str(equipe_id))
+        # Fin débogage
+        b2b = False
+        handicap[equipe_id]=0
+        nombre_matchs = 1
+        for match in four_last_days:
+            # Calcul du nombre de fois où l'équipe apparait dans le calendrier des 4 derniers jours
+            if equipe_id == match['team_home'] or equipe_id == match['team_visitor']:
+                nombre_matchs += 1
+            # L'équipe a-t-elle joué la veille ?
+            if match['team_home'] == equipe_id and match['date'] == date_veille:
+                b2b = True
+            elif match['team_visitor'] == equipe_id and match['date'] == date_veille:
+                b2b = True    
+
+        # Calcul du nombre de points à retirer en fonction de la valeur des différentes variables
+        if b2b:
+            handicap[equipe_id] -= 20
+        if nombre_matchs > 1 and nombre_matchs < 3:
+            handicap[equipe_id] -= 10
+        if nombre_matchs > 2:
+            handicap[equipe_id] -= 20
+
+    return handicap
+
